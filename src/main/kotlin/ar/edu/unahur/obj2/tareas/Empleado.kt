53ac14213ddbe5d3022ad6d52ea46cd945d8afe7
VallesFranco <vallesfrancodavid@gmail.com>
2021-06-04 16:02:42 -0300

    Código hasta el requerimiento 3, faltan tests.

---
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Empleado.kt
@@ -0,0 +1,4 @@
+package ar.edu.unahur.obj2.tareas
+
+class Empleado(val dineroPorHoraTrabajada: Int) {
+}
--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,20 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+class Tarea(val horasEstimadas: Int, val responsable: Empleado, val empleados: MutableList<Empleado>,
+            val costoDeInfraestructura: Int) {
+
+    fun nominaDeEmpleados(): List<Empleado> {
+        val nomina = empleados
+        nomina.add(responsable)
+        return nomina
+    }
+
+    fun horasNecesariasParaFinalizar() = horasEstimadas / empleados.size
+
+    fun costo() = horasNecesariasParaFinalizar() * sueldoPorHoraDeCadaEmpleado() +
+            horasEstimadas * responsable.dineroPorHoraTrabajada + costoDeInfraestructura
+
+    fun sueldoPorHoraDeCadaEmpleado() = empleados.sumBy { it.dineroPorHoraTrabajada }
+
+    
+}
